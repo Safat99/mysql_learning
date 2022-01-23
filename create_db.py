2557a397de271821b502321109be5dbf815cda58
Safat99 <sarkersafat99@gmail.com>
2022-01-23 12:03:05 +0600

    started with python_mysql.connector

---
+++ create_db.py
@@ -0,0 +1,16 @@
+import mysql.connector as sql
+
+mydb = sql.connect(
+    host = 'localhost',
+    user = 'root',
+    password = 'hala_madrid@123',
+    database = 'test_db'
+)
+
+cursor = mydb.cursor()
+
+# cursor.execute("CREATE DATABASE test_db")
+cursor.execute("SHOW DATABASES")
+
+for i in cursor:
+    print(i)
---
+++ create_table.py
@@ -0,0 +1,12 @@
+import mysql.connector as sql
+
+mydb = sql.connect(
+    host = 'localhost',
+    user = 'root',
+    password = 'hala_madrid@123',
+    database = 'test_db'
+)
+
+cursor = mydb.cursor()
+
+cursor.execute("CREATE TABLE customers (name VARCHAR(20), address VARCHAR(100))")
